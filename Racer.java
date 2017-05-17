eb66ecf89bfbe621922d63e771d8380e88b91d45
SiliconUnicorn <micahhansonbrook@icloud.com>
2017-05-17 19:58:12 -0400

    Add files via upload

--- Racer.java
+++ Racer.java
@@ -1,15 +1,9 @@
 /**
  * The Racer class will be able to move throughout the screen
  * 
- * @author Silicon Unicorn
+ * @author Micah Hansonbrook
  * @version 0.1
  */
-public class Racer{
-    int maximumHealth=20000;
-    int currentHealth;
-    int deltaSpeed;
-    int deltaRotation;
-    public float getHealthPercentage(){
-        return (float)currentHealth/(float)maximumHealth;
-    }
+public class Racer extends Actor{
+    
 }
