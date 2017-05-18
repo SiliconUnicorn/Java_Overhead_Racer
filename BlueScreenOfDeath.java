996398383d31eff1c61b0e4e9209a246dd98874f
SeagullSoars <zhsiegel@gmail.com>
2017-05-18 11:09:45 -0400

    Update BlueScreenOfDeath.java

--- BlueScreenOfDeath.java
+++ BlueScreenOfDeath.java
@@ -4,15 +4,20 @@
 @author Seagull Soars and Silicon Unicorn
 version 0.1
 */
+private int numberOfOpponentRacers=10;
 public class BlueScreenOfDeath{
+  
+  }
+public void act(){
   setupRacers();
   setUpTrack();
-  public void setupRacers(int numberOfOpponentRacers){
-      while(numberOfOpponentRacers > 0){
-        
-  }
+   }
   public void setupTrack(){
     
   }
+public void setupRacers(int numberOfOpponentRacers){
+      while(numberOfOpponentRacers > 0){
+      OpponentRacer opponentRacer = new OpponentRacer();
+      addObject(new opponentRacer(), _x, _y); 
 }
   
