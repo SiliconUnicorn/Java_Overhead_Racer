185073af9b1a00ea64b0bfecb4fe42e5b5d74a2f
SiliconUnicorn <micahhansonbrook@icloud.com>
2017-05-17 20:08:17 -0400

    Update Racer.java

--- Racer.java
+++ Racer.java
@@ -1,9 +1,26 @@
 /**
  * The Racer class will be able to move throughout the screen
  * 
- * @author Micah Hansonbrook
+ * @author Silicon Unicorn
  * @version 0.1
  */
-public class Racer extends Actor{
-    
+public class Racer{
+    int maximumHealth=20000;
+    int currentHealth;
+    int deltaSpeed;
+    int deltaRotation;
+    public float getHealthPercentage(){
+        return (float)currentHealth/(float)maximumHealth;
+    }
+    public class linuxRacer extends Racer{
+       int currentSpeed=0;
+       int turningVariant=1;
+       if (isKeyDown("up")==true){
+            move(currentSpeed);
+            currentSpeed=currentSpeed+1;
+       if (currentSpeed=10*turningVariant){
+           deltaRotation=deltaRotation-1;
+           turningVariant=turningVariant+1;
+       }
+    }
 }
